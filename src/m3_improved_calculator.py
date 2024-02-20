bbeef79d07aeeafe27bad4b3d64baf2d8e2a6069
iahearn001 <iahearn001@student.franciscan.edu>
2024-02-20 10:33:29 -0500

    completed all

--- src/m3_improved_calculator.py
+++ src/m3_improved_calculator.py
@@ -1,5 +1,44 @@
+import math
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+        return x / y
+def exponentiate(x, y):
+    return math.pow(x, y)
+
+def if_calc():
+    print("Welcome to the Simple Calculator!")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print("Choose operation:")
+    print("(+) Add")
+    print("(-) Subtract")
+    print("(*) Multiply")
+    print("(/) Divide")
+    operation = input("Enter operation: ")
+    
+    if operation == '+':
+        print("Add:", add(num1, num2))
+    elif operation == '-':
+        print("Subtract:", subtract(num1, num2))
+    elif operation == '*':
+        print("Multiply:", multiply(num1, num2))
+    elif operation == '/':
+        print("Divide:", divide(num1, num2))
+    else:
+        print("Invalid Operation!")
+    
+    print("Thank you for using the Simple Calculator. Goodbye!")
+if_calc()
 ###############################################################################
-# TODO: 1. (4 pts)
+# DONE: 1. (4 pts)
 #
 #   In this module, we will improve upon the calculator that we built in the
 #   Session 5 coding exercises.
@@ -41,7 +80,7 @@
 ###############################################################################
 
 ###############################################################################
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Now, do the same thing that you did in _TODO_ 1, but this time, use case
 #   statements in your solution instead of if statements.
@@ -51,3 +90,24 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+def case_calc():
+    print("Welcome to the Simple Calculator!")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print("Choose operation:")
+    print("(+) Add")
+    print("(-) Subtract")
+    print("(*) Multiply")
+    print("(/) Divide")
+    operation = input("Enter operation: ")
+    
+    result = {
+        '+': add(num1, num2),
+        '-': subtract(num1, num2),
+        '*': multiply(num1, num2),
+        '/': divide(num1, num2)
+    }.get(operation, "Invalid Operation!")
+    
+    print(result)
+    print("Thank you for using the Simple Calculator. Goodbye!")
+case_calc()
